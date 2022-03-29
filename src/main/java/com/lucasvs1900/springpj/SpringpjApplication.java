5885951530c2e59cbaf68163078832ffbe53280f
lucas <lucasvs1900@gmail.com>
2022-03-29 17:25:52 -0300

    create pj

---
+++ src/main/java/com/lucasvs1900/springpj/SpringpjApplication.java
@@ -0,0 +1,13 @@
+package com.lucasvs1900.springpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasvs1900/springpj/SpringpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasvs1900.springpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
